2f359e8a2667c5e1d7b35b3af8a07386a219f413
Karthick <karthickk1996@hotmail.com>
2026-01-13 19:48:05 +0530

    chore: remove empty task.go file from server/tasks

--- internal/modules/server/tasks/task.go
+++
@@ -1,6 +0,0 @@
-// Package tasks provides server task definitions for remote execution.
-//
-// All task factory functions return *taskrunner.BaseTask for consistency.
-// Use the taskrunner package for shell helpers like WrapScript(), ShellDefaults(),
-// CommonFunctions(), and AptFunctions().
-package tasks
