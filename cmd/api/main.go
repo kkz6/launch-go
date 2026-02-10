7aa5924b24ab1b45779584458a4fe8f35a9480be
Karthick <karthickk1996@hotmail.com>
2026-02-10 18:23:14 +0530

    feat: add security headers middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -159,6 +159,7 @@ func (a *Application) registerMiddleware() {
 		AllowHeaders:     "Origin,Content-Type,Accept,Authorization,X-Team-ID",
 		AllowCredentials: true,
 	}))
+	a.fiber.Use(middleware.SecurityHeaders())
 	a.fiber.Use(middleware.RequestLogger(a.logger, a.config.App.Environment == "development"))
 }
 
